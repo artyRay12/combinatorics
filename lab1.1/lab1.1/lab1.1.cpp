ab92c672145c0bc943f16c9e08983be7e1c76aae
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2020-02-24 12:53:57 +0300

    test

---
+++ lab1.1/lab1.1/lab1.1.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <boost/timer.hpp>
+#include <vector>
+#include <ostream>
+#include <algorithm>
+#include <iterator>
+#include <cstddef>
+
+using namespace std;
+using namespace boost;
+
+int main()
+{
+    vector<size_t> v = { 7, 1, 12, 5, 9, 20, 15 };
+    timer t;
+    t.restart();
+
+    sort(v.begin(), v.end());
+
+    do
+    {
+        copy(v.begin(), v.end(), ostream_iterator<size_t>(cout, " "));
+        cout << endl;
+    } while (next_permutation(v.begin(), v.end()));
+
+    double duration = t.elapsed();
+    cout << duration << endl;
+
+    return 0;
+}
---
+++ lab1.2/lab1.2/lab1.2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <boost/timer.hpp>
+
+using namespace std;
+using namespace boost;
+
+const int N = 5;
+
+int main()
+{
+	int m = N + 1;
+	boost::timer t;
+	std::vector<size_t> p(N + 2), r(N + 1);
+	std::vector<int> d(N + 1);
+
+	for (int i = 1; i < r.size(); i++)
+	{
+		p[i] = r[i] = i;
+		d[i] = -1;
+	}
+	d[1] = 0;
+	p[0] = p[N + 1] = m;
+
+	t.restart();
+	while (m != 1)
+	{
+		//copy(p.begin() + 1, p.end() - 1, std::ostream_iterator<size_t>(std::cout, " "));
+		//std::cout << std::endl;
+		m = N;
+
+		while (p[r[m] + d[m]] > m)
+		{
+			d[m] = -d[m];
+			m = m - 1;
+		}
+
+		std::swap(p[r[m]], p[r[m] + d[m]]);
+		std::swap(r[p[r[m]]], r[m]);
+	}
+
+	double duration = t.elapsed();
+	cout << duration << std::endl;
+
+	return 0;
+}
