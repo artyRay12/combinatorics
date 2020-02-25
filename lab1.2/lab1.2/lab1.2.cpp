9db6ea82ee60aef442f1e7111f04f1eea55f2bbb
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2020-02-25 20:11:15 +0300

    test

--- lab1.2/lab1.2/lab1.2.cpp
+++ lab1.2/lab1.2/lab1.2.cpp
@@ -8,14 +8,24 @@ using namespace std;
 using namespace boost;
 
 
+void printVector(vector<int> v)
+{
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << "   ";
+
+	}
+	cout << endl;
+}
+
 const int N = 5;
 
 int main()
 {
 	int m = N + 1;
 	boost::timer t;
-	std::vector<size_t> p(N + 2), r(N + 1);
-	std::vector<int> d(N + 1);
+	vector<int> p(N + 2), r(N + 1);
+	vector<int> d(N + 1);
 
 	for (int i = 1; i < r.size(); i++)
 	{
@@ -25,11 +35,18 @@ int main()
 	d[1] = 0;
 	p[0] = p[N + 1] = m;
 
+	cout << "p: "; printVector(p);
+	cout << "r: "; printVector(r);
+	cout << "d: "; printVector(d);
+	system("pause");
+
+
+
 	t.restart();
 	while (m != 1)
 	{
-		//copy(p.begin() + 1, p.end() - 1, std::ostream_iterator<size_t>(std::cout, " "));
-		//std::cout << std::endl;
+		copy(p.begin() + 1, p.end() - 1, std::ostream_iterator<size_t>(std::cout, " "));
+		cout << std::endl;
 		m = N;
 
 		while (p[r[m] + d[m]] > m)
@@ -42,8 +59,7 @@ int main()
 		std::swap(r[p[r[m]]], r[m]);
 	}
 
-	double duration = t.elapsed();
-	cout << duration << std::endl;
+	cout << t.elapsed() << std::endl;
 
 	return 0;
 }
