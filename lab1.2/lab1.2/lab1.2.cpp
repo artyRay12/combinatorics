6bc068e3c6febd9ae4215cdca016416998bbeb55
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2020-02-26 03:48:55 +0300

    test

--- lab1.2/lab1.2/lab1.2.cpp
+++ lab1.2/lab1.2/lab1.2.cpp
@@ -2,6 +2,9 @@
 #include <algorithm>
 #include <iterator>
 #include <vector>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
 #include <boost/timer.hpp>
 
 using namespace std;
@@ -12,16 +15,22 @@ void printVector(vector<int> v)
 {
 	for (int i = 0; i < v.size(); i++)
 	{
-		cout << v[i] << "   ";
+		if (v[i] < 0)
+			cout << v[i] << "  ";
+		else
+			cout << v[i] << "   ";
+			
 
 	}
 	cout << endl;
 }
 
-const int N = 5;
+
+
 
 int main()
 {
+	int N = 5;
 	int m = N + 1;
 	boost::timer t;
 	vector<int> p(N + 2), r(N + 1);
@@ -35,28 +44,39 @@ int main()
 	d[1] = 0;
 	p[0] = p[N + 1] = m;
 
+	t.restart();
 	cout << "p: "; printVector(p);
 	cout << "r: "; printVector(r);
 	cout << "d: "; printVector(d);
-	system("pause");
-
-
-
-	t.restart();
 	while (m != 1)
 	{
-		copy(p.begin() + 1, p.end() - 1, std::ostream_iterator<size_t>(std::cout, " "));
-		cout << std::endl;
-		m = N;
+		//copy(p.begin() + 1, p.end() - 1, std::ostream_iterator<size_t>(std::cout, " "));
+		system("cls");
+		cout << "p: "; printVector(p);
+		cout << "r: "; printVector(r);
+		cout << "d: "; printVector(d);
+		system("pause");
+		cout << endl;
 
+
+		m = N;
 		while (p[r[m] + d[m]] > m)
 		{
 			d[m] = -d[m];
 			m = m - 1;
+
+
+
+
+
+
+
+
+
 		}
 
-		std::swap(p[r[m]], p[r[m] + d[m]]);
-		std::swap(r[p[r[m]]], r[m]);
+		swap(p[r[m]], p[r[m] + d[m]]);
+		swap(r[p[r[m]]], r[m]);
 	}
 
 	cout << t.elapsed() << std::endl;
