a285b0f62a1ce380ff08ec4b4208227e62ae1b52
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2020-02-26 04:46:13 +0300

    added 1.3

--- lab1.3/lab1.3/lab1.3.cpp
+++ lab1.3/lab1.3/lab1.3.cpp
@@ -1,20 +1,58 @@
-﻿// lab1.3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
+#include <cstdlib> 
+using namespace std;
 
-#include <iostream>
+const int DECK_SIZE = 10;
 
-int main()
+int getRandomNumber(int min, int max)
 {
-    std::cout << "Hello World!\n";
+	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+	return static_cast<int>(rand() * fraction * (max - min + 1) + min);
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void shuffleDeck(vector<size_t>& deck)
+{
+	for (int i = 0; i <= DECK_SIZE; i++)
+	{
+		swap(deck[i], deck[getRandomNumber(0, DECK_SIZE)]);
+	}
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+int main()
+{	
+	vector<size_t> deck;
+	for (int i = 0; i <= DECK_SIZE; i++)
+	{
+		deck.push_back(i);	
+	}
+	
+	int buttonCode = 0;
+	while (1)
+	{
+
+		copy(deck.begin(), deck.end() - 1, std::ostream_iterator<size_t>(cout, " "));
+		cout << "\npress 'R' for shuffle" << endl;
+		cout << endl;
+
+		buttonCode = _getch();
+		
+		if (buttonCode == 114)
+		{
+			shuffleDeck(deck);
+		}
+		else
+		{
+			break;
+		}
+	}
+	return 0;
+	
+}
+
+
