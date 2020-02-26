8f2091fc0109d37dc416a98d611dab9029685d2a
artyRay12 <39522767+artyRay12@users.noreply.github.com>
2020-02-26 06:45:19 +0300

    test

--- lab1.2/lab1.2/lab1.2.cpp
+++ lab1.2/lab1.2/lab1.2.cpp
@@ -10,24 +10,6 @@
 using namespace std;
 using namespace boost;
 
-
-void printVector(vector<int> v)
-{
-	for (int i = 0; i < v.size(); i++)
-	{
-		if (v[i] < 0)
-			cout << v[i] << "  ";
-		else
-			cout << v[i] << "   ";
-			
-
-	}
-	cout << endl;
-}
-
-
-
-
 int main()
 {
 	int N = 5;
@@ -45,36 +27,18 @@ int main()
 	p[0] = p[N + 1] = m;
 
 	t.restart();
-	cout << "p: "; printVector(p);
-	cout << "r: "; printVector(r);
-	cout << "d: "; printVector(d);
 	while (m != 1)
 	{
-		//copy(p.begin() + 1, p.end() - 1, std::ostream_iterator<size_t>(std::cout, " "));
-		system("cls");
-		cout << "p: "; printVector(p);
-		cout << "r: "; printVector(r);
-		cout << "d: "; printVector(d);
-		system("pause");
+		copy(p.begin() + 1, p.end() - 1, std::ostream_iterator<size_t>(std::cout, " "));
 		cout << endl;
-
-
+		system("pause");
 		m = N;
 		while (p[r[m] + d[m]] > m)
 		{
 			d[m] = -d[m];
 			m = m - 1;
 
-
-
-
-
-
-
-
-
 		}
-
 		swap(p[r[m]], p[r[m] + d[m]]);
 		swap(r[p[r[m]]], r[m]);
 	}
